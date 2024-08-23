e2ac81dbc875958bbb61bbe7929727d099b2b41f
ItsKader <96780401+ItsKader@users.noreply.github.com>
2024-08-23 15:58:26 -0400

    Add files via upload
    app front-end

---
+++ front-end/src/layouts/HomePage/components/LibraryServices.tsx
@@ -0,0 +1,18 @@
+export const LibraryServices = () => {
+  return (
+    <div className="container my-5">
+      <div className="row p-4 align-items-center border shadow-lg">
+        <div className="col-lg-7 p-3">
+          <h1 className="display-4 fw-bold">
+            Can't find what you are looking for?
+          </h1>
+          <p className="lead">
+            If you cannot find what you are looking for, contact our admin by email at abdel.kapi@mail.mcgill.ca.
+          </p>
+          
+        </div>
+        <div className="col-lg-4 offset-lg-1 shadow-lg lost-image"></div>
+      </div>
+    </div>
+  );
+};
